6d90eb9529990b572bd1f58dd93c4a9e8ba4f009
iantendick <itendick@placester.com>
2012-10-03 11:22:53 -0400

    Onboard theme opts separated out into flat file for easy configuration...

--- config/customizer/onboard_theme_opts.php
+++ config/customizer/onboard_theme_opts.php
@@ -3,333 +3,181 @@
 global $PL_CUSTOMIZER_ONBOARD_OPTS;
 $PL_CUSTOMIZER_ONBOARD_OPTS = array( 
 
-        //         array(
-        //             [name] => 'Titles & Logo'
-        //             [type] => heading
-        //         ),
+        array(
+            'name' => 'Title & Logo',
+            'type' => 'heading'
+        ),
 
-        //         array(
-        //             [name] => Site Title
-        //             [desc] => Site title in header.
-        //             [id] => pls-site-title
-        //             [type] => text
-        //         ),
+        array(
+            'name' => 'Site Title',
+            'desc' => 'Site title in header.',
+            'id' => 'pls-site-title',
+            'type' => 'text'
+        ),
 
-        //         array(
-        //             [name] => Site Subtitle
-        //             [desc] => Site subtitle in header.
-        //             [id] => pls-site-subtitle
-        //             [type] => text
-        //         ),
+        array(
+            'name' => 'Site Subtitle',
+            'desc' => 'Site subtitle in header.',
+            'id' => 'pls-site-subtitle',
+            'type' => 'text'
+        ),
 
-        //         array(
-        //             [name] => Site Logo
-        //             [desc] => Upload your logo here. It will appear in the header and will override the title you've provided above.
-        //             [id] => pls-site-logo
-        //             [type] => upload
-        //         ),
+        array(
+            'name' => 'Site Logo',
+            'desc' => 'Upload your logo here. It will appear in the header and will override the title you\'ve provided above.',
+            'id' => 'pls-site-logo',
+            'type' => 'upload'
+        ),
 
-        //         array(
-        //             [name] => Google Analytics Tracking Code
-        //             [desc] => Add your google analytics tracking ID code here. It looks something like this: UA-XXXXXXX-X
-        //             [id] => pls-google-analytics
-        //             [type] => text
-        //         ),
+        array(
+            'name' => 'Contact Info',
+            'type' => 'heading'
+        ),
 
-        //         array(
-        //             [name] => Contact Information
-        //             [type] => heading
-        //         ),
+        array(
+            'name' => 'Your First and Last Name',
+            'desc' => 'Add the name you want to display on the site.',
+            'id' => 'pls-user-name',
+            'type' => 'text'
+        ),
 
-        //         array(
-        //             [name] => Your First and Last Name
-        //             [desc] => Add the name you want to display on the site.
-        //             [id] => pls-user-name
-        //             [type] => text
-        //         ),
+        array(
+            'name' => 'Your Email Address',
+            'desc' => 'Add the email address you want to display on the site.',
+            'id' => 'pls-user-email',
+            'type' => 'text'
+        ),
 
-        //     [12] => Array
-        //         (
-        //             [name] => Your Email Address
-        //             [desc] => Add the email address you want to display on the site.
-        //             [id] => pls-user-email
-        //             [type] => text
-        //         )
+        array(
+            'name' => 'Your Phone Number',
+            'desc' => 'Add the phone you want to display on the site.',
+            'id' => 'pls-user-phone',
+            'type' => 'text'
+        ),
 
-        //     [13] => Array
-        //         (
-        //             [name] => Your Phone Number
-        //             [desc] => Add the phone you want to display on the site.
-        //             [id] => pls-user-phone
-        //             [type] => text
-        //         )
+        array(
+            'name' => 'Your Bio',
+            'desc' => 'Add your bio that you want to display on the site.',
+            'id' => 'pls-user-description',
+            'type' => 'textarea'
+        ),
 
-        //     [14] => Array
-        //         (
-        //             [name] => Your Bio
-        //             [desc] => Add your bio that you want to display on the site.
-        //             [id] => pls-user-description
-        //             [type] => textarea
-        //         )
+        array(
+            'name' => 'Your Headshot',
+            'desc' => 'Add your headshot that you want to display on the site.',
+            'id' => 'pls-user-image',
+            'type' => 'upload'
+        ),
 
-        //     [15] => Array
-        //         (
-        //             [name] => Your Headshot
-        //             [desc] => Add your headshot that you want to display on the site.
-        //             [id] => pls-user-image
-        //             [type] => upload
-        //         )
+        array(
+            'name' => 'Company Info',
+            'type' => 'heading'
+        ),
 
-        //     [16] => Array
-        //         (
-        //             [name] => Company Info
-        //             [desc] => 
-        //             [type] => info
-        //         )
+        array(
+            'name' => 'Company Name',
+            'desc' => 'Add your company\'s name you want to display on the site.',
+            'id' => 'pls-company-name',
+            'type' => 'text'
+        ),
 
-        //     [17] => Array
-        //         (
-        //             [name] => Company Name
-        //             [desc] => Add your company's name you want to display on the site.
-        //             [id] => pls-company-name
-        //             [type] => text
-        //         )
+        array(
+            'name' => 'Company Phone',
+            'desc' => 'Add your company\'s phone number you want to display on the site.',
+            'id' => 'pls-company-phone',
+            'type' => 'text'
+        ),
 
-        //     [18] => Array
-        //         (
-        //             [name] => Company Phone
-        //             [desc] => Add your company's phone number you want to display on the site.
-        //             [id] => pls-company-phone
-        //             [type] => text
-        //         )
+        array(
+            'name' => 'Company Email',
+            'desc' => 'Add your company\'s email address you want to display on the site.',
+            'id' => 'pls-company-email',
+            'type' => 'text'
+        ),
 
-        //     [19] => Array
-        //         (
-        //             [name] => Company Email
-        //             [desc] => Add your company's email address you want to display on the site.
-        //             [id] => pls-company-email
-        //             [type] => text
-        //         )
+        array(
+            'name' => 'Company Street Address',
+            'desc' => 'Add your company\'s street address you want to display on the site.',
+            'id' => 'pls-company-street',
+            'type' => 'text'
+        ),
 
-        //     [20] => Array
-        //         (
-        //             [name] => Company Street Address
-        //             [desc] => Add your company's street address you want to display on the site.
-        //             [id] => pls-company-street
-        //             [type] => text
-        //         )
+        array(
+            'name' => 'Company City / Locality',
+            'desc' => 'Add your company\'s city/locality location you want to display on the site.',
+            'id' => 'pls-company-locality',
+            'type' => 'text'
+        ),
 
-        //     [21] => Array
-        //         (
-        //             [name] => Company City / Locality
-        //             [desc] => Add your company's city/locality location you want to display on the site.
-        //             [id] => pls-company-locality
-        //             [type] => text
-        //         )
+        array(
+            'name' => 'Company State / Region',
+            'desc' => 'Add your company\'s state/region location you want to display on the site.',
+            'id' => 'pls-company-region',
+            'type' => 'text'
+        ),
 
-        //     [22] => Array
-        //         (
-        //             [name] => Company State / Region
-        //             [desc] => Add your company's state/region location you want to display on the site.
-        //             [id] => pls-company-region
-        //             [type] => text
-        //         )
+        array(
+            'name' => 'Company Postal Code',
+            'desc' => 'Add your company\'s postal code you want to display on the site.',
+            'id' => 'pls-company-postal',
+            'type' => 'text'
+        ),
 
-        //     [23] => Array
-        //         (
-        //             [name] => Company Postal Code
-        //             [desc] => Add your company's postal code you want to display on the site.
-        //             [id] => pls-company-postal
-        //             [type] => text
-        //         )
+        array(
+            'name' => 'Company Description',
+            'desc' => 'Add your company\'s description you want to display on the site.',
+            'id' => 'pls-company-description',
+            'type' => 'textarea'
+        ),
 
-        //     [24] => Array
-        //         (
-        //             [name] => Company Description
-        //             [desc] => Add your company's description you want to display on the site.
-        //             [id] => pls-company-description
-        //             [type] => textarea
-        //         )
+        array(
+            'name' => 'Styles & Coloring',
+            'type' => 'heading'
+        ),
 
-        //     [25] => Array
-        //         (
-        //             [name] => Search Options
-        //             [type] => heading
-        //         )
+        // array(
+        //     'name' => 'Site Background',
+        //     'desc' => 'Change the site\'s background.',
+        //     'id' => 'site_background',
+        //     'selector' => 'body',
+        //     'type' => 'background'
+        // ),
 
-        //     [26] => Array
-        //         (
-        //             [name] => Price Range Options
-        //             [desc] => Use the controls below to change what options are displayed when users click the bedrooms filer
-        //             [type] => info
-        //         )
+        // array(
+        //     'name' => 'Inner Background',
+        //     'desc' => 'Change the site\'s inner background.',
+        //     'id' => 'inner_background',
+        //     'selector' => '.inner',
+        //     'type' => 'background'
+        // ),
 
-        //     [27] => Array
-        //         (
-        //             [name] => Min Search Price
-        //             [desc] => 
-        //             [id] => pls-option-price-min
-        //             [std] => 0
-        //             [type] => text
-        //         )
+        array(
+            'name' => 'H1 Title',
+            'desc' => 'Change main site title\'s size, font-family, styling, and color.',
+            'id' => 'h1_title',
+            'selector' => 'header h1 a',
+            'type' => 'typography'
+        ),
 
-        //     [28] => Array
-        //         (
-        //             [name] => Max Search Price
-        //             [desc] => 
-        //             [id] => pls-option-price-max
-        //             [std] => 1000000
-        //             [type] => text
-        //         )
+        array(
+            'name' => 'H2 Subtitle',
+            'desc' => 'Change the site subtitle\'s size, font-family, styling, and color.',
+            'id' => 'h2_subtitle',
+            'selector' => 'header h2',
+            'type' => 'typography'
+        ),
 
-        //     [29] => Array
-        //         (
-        //             [name] => Price Increment
-        //             [desc] => 
-        //             [std] => 50000
-        //             [id] => pls-option-price-inc
-        //             [type] => text
-        //         )
+        array(
+            'name' => 'Analytics',
+            'type' => 'heading'
+        ),
 
-        //     [30] => Array
-        //         (
-        //             [name] => Bedroom Options
-        //             [desc] => Use the controls below to change what options are displayed when users click the bedrooms filter
-        //             [type] => info
-        //         )
-
-        //     [31] => Array
-        //         (
-        //             [name] => Bedroom Options Start
-        //             [desc] => 
-        //             [id] => pls-option-bed-min
-        //             [std] => 0
-        //             [type] => text
-        //         )
-
-        //     [32] => Array
-        //         (
-        //             [name] => Bedroom Options End
-        //             [desc] => 
-        //             [id] => pls-option-bed-max
-        //             [std] => 15
-        //             [type] => text
-        //         )
-
-        //     [33] => Array
-        //         (
-        //             [name] => Bathroom Options
-        //             [desc] => Use the controls below to change what options are displayed when users click the bathrooms filter
-        //             [type] => info
-        //         )
-
-        //     [34] => Array
-        //         (
-        //             [name] => Bathroom Options Start
-        //             [desc] => 
-        //             [id] => pls-option-bath-min
-        //             [std] => 0
-        //             [type] => text
-        //         )
-
-        //     [35] => Array
-        //         (
-        //             [name] => Bathroom Options End
-        //             [desc] => 
-        //             [id] => pls-option-bath-max
-        //             [std] => 10
-        //             [type] => text
-        //         )
-
-        //     [36] => Array
-        //         (
-        //             [name] => Half Bath Options
-        //             [desc] => Use the controls below to change what options are displayed when users click the bathrooms filter
-        //             [type] => info
-        //         )
-
-        //     [37] => Array
-        //         (
-        //             [name] => Half Bath Options Start
-        //             [desc] => 
-        //             [id] => pls-option-half-bath-min
-        //             [std] => 0
-        //             [type] => text
-        //         )
-
-        //     [38] => Array
-        //         (
-        //             [name] => Half Bath Options End
-        //             [desc] => 
-        //             [id] => pls-option-half-bath-max
-        //             [std] => 5
-        //             [type] => text
-        //         )
-
-        //     [39] => Array
-        //         (
-        //             [name] => Color Styles
-        //             [type] => heading
-        //         )
-
-        //     [40] => Array
-        //         (
-        //             [name] => Site Background
-        //             [desc] => Change the site's background.
-        //             [id] => site_background
-        //             [selector] => body
-        //             [type] => background
-        //         )
-
-        //     [41] => Array
-        //         (
-        //             [name] => Inner Background
-        //             [desc] => Change the site's inner background.
-        //             [id] => inner_background
-        //             [selector] => .inner
-        //             [type] => background
-
-        //     [44] => Array
-        //         (
-        //             [name] => Colors & Styling
-        //             [type] => heading
-        //         )
-
-        //     [45] => Array
-        //         (
-        //             [name] => H1 Title
-        //             [desc] => Change main site title's size, font-family, styling, and color.
-        //             [id] => h1_title
-        //             [std] => Array
-        //                 (
-        //                     [color] => 
-        //                     [image] => 
-        //                     [repeat] => 
-        //                     [position] => 
-        //                     [attachment] => scroll
-        //                 )
-
-        //             [selector] => header h1 a
-        //             [type] => typography
-        //         )
-
-        //     [46] => Array
-        //         (
-        //             [name] => H2 Subtitle
-        //             [desc] => Change the site subtitle's size, font-family, styling, and color.
-        //             [id] => h2_subtitle
-        //             [std] => Array
-        //                 (
-        //                     [color] => 
-        //                     [image] => 
-        //                     [repeat] => 
-        //                     [position] => 
-        //                     [attachment] => scroll
-        //                 )
-
-        //             [selector] => header h2
-        //             [type] => typography
-        //         )
+        array(
+            'name' => 'Google Analytics Tracking Code',
+            'desc' => 'Add your google analytics tracking ID code here. It looks something like this: UA-XXXXXXX-X',
+            'id' => 'pls-google-analytics',
+            'type' => 'text'
+        )
 
         );
 ?>
