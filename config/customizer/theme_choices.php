9893db84cce39d7049889f0c100b4be3e180335f
iantendick <itendick@placester.com>
2012-11-04 19:18:46 -0500

    Updating selectors based on QA

--- config/customizer/theme_choices.php
+++ config/customizer/theme_choices.php
@@ -39,13 +39,13 @@ $PL_CUSTOMIZER_THEME_INFO = array(
 													 'pls-site-title' => 'header h1 a',
 													 'pls-site-subtitle' => 'header h2',
 													 'pls-user-email' => 'header .phone a, footer .phone a, .widget-pls-agent .email',
-													 'pls-user-phone' => 'header .phone, footer .phone, .widget-pls-agent .phone'	
+													 'pls-user-phone' => '.widget-pls-agent .phone' // 'header .phone, footer .phone, .widget-pls-agent .phone'	
 													 ),
 										'manchester' => array(
 													 'pls-site-title' => 'header h1 a',
 													 'pls-site-subtitle' => 'header #slogan',
-													 'pls-user-email' => 'header .email a, footer .footer-contact a, .widget-pls-agent .email',
-													 'pls-user-phone' => 'header .phone, .widget-pls-agent .phone'	
+													 'pls-user-email' => 'header .email a, footer .footer-contact a, .widget-pls-agent .nrm-txt a',
+													 'pls-user-phone' => 'header .phone, .widget-pls-agent .phone'
 													 ),
 										'tampa' 	 => array(
 													 'pls-site-title' => 'header h1 a',
@@ -56,8 +56,8 @@ $PL_CUSTOMIZER_THEME_INFO = array(
 										'ventura' 	 => array(
 													 'pls-site-title' => 'header h1 a',
 													 'pls-site-subtitle' => 'header h2',
-													 'pls-user-email' => 'header .email li a, footer footer-contact p.info a, .widget-pls-agent .email',
-													 'pls-user-phone' => 'header .phone li.phone-bg-mid, footer footer-contact p.info strong .widget-pls-agent .phone'	
+													 'pls-user-email' => 'header .email li a, footer #footer-contact p.info a, .widget-pls-agent .email',
+													 'pls-user-phone' => 'header .phone li.phone-bg-mid, footer #footer-contact p.info strong, .widget-pls-agent .phone'	
 													 )
 							 		);
 
