5191eca77b67bf30345ca1d6486361a0018e1014
Galen Vinter <galen.vinter@gmail.com>
2012-12-03 15:44:22 -0500

    separating menus to create, and ones that conflict.
    Signed-off-by: Galen Vinter <galen.vinter@gmail.com>

--- lib/menus.php
+++ lib/menus.php
@@ -20,36 +20,72 @@ class PL_Menus {
     // Get All current menus
     $all_menus = wp_get_nav_menus();
     
+    $conflicting_menus = array();
+    $menus_to_create = array();
+    
+    // Check manifest menus against current menus
     foreach ($menus as $menu) {
 
       // check if $menu exists
       $menu_check = wp_get_nav_menu_object( $menu['name'] );
 
-      // If menu doesn't exist, create it
       if ( empty($menu_check) ) {
-        wp_update_nav_menu_object( 0, array(
-          'menu-name' => $menu['name'])
-        );
+          // If menu doesn't exist, create it
+          wp_update_nav_menu_object( 0, array(
+            'menu-name' => $menu['name'])
+          );
+          
+          self::add_pages_to_menu_by_name($menu);
+          
+          
+      } else {
+          // if it already exists, add to array to ask user if we can delete them
+          $conflicting_menus .= $menu;
       }
-      
-      // if it already exists, ask user if we can delete
-      
-        // if user says Go, then Go
-      
-      
-      
-      // once menu is create, add_pages_to_menu_by_name()
-      
-      // assign_menu_to_theme_location ()
       
     }
 
+    // if conflicting menus exist, resolve by asking user
+    if ( !empty($conflicting_menus) ) {
+      
+    }
+    
+    // if user says Go, then add menus to $menus_to_create
+
+    
+    // once menu is create, add_pages_to_menu_by_name()
+    
+    // assign_menu_to_theme_location ()
+    
   }
 
   function add_pages_to_menu_by_name ($menu) {
     
-    // if page already exists, skip
+    foreach ($menu['pages'] as $page) {
     
+    var_dump("add_pages_to_menu is firing");
+
+      wp_update_nav_menu_item($menu_id, 0, array(
+              'menu-item-title' =>  __($menu['pages']),
+              // 'menu-item-classes' => 'forums',
+              // 'menu-item-url' => home_url( '/forums/' ), 
+              'menu-item-status' => 'publish'));
+
+    }
+    
+    // Insert new page 
+    // $page = wp_insert_post(array(
+    //   'post_title' => 'Blog', 
+    //   'post_content' => '', 
+    //   'post_status' => 'publish', 
+    //   'post_type' => 'page')); 
+
+    // Insert new nav_menu_item 
+    // $nav_item = wp_insert_post(array(
+    //   'post_title' => 'News', 
+    //   'post_content' => '', 
+    //   'post_status' => 'publish', 
+    //   'post_type' => 'nav_menu_item'));
   }
 
   function assign_menu_to_theme_location ($menu) {
