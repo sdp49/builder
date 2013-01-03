454cdce089dfbcf5ca3b390cc91a968c45fb4702
mpeshev <mario@peshev.net>
2013-01-03 21:12:25 +0200

    Typo for consumer key to secret

--- lib/twitteroauth/config.php
+++ lib/twitteroauth/config.php
@@ -8,7 +8,7 @@
 #define('CONSUMER_KEY', '1OYM9QH9uEfX1tuh6hTfw');
 define('CONSUMER_KEY', 'a5Jihv3R0km2IsZmacsljQ');
 #define('CONSUMER_SECRET', '1dFNyFoJiCB8SmrPwZ9r1yNIBgcDbnsVnxFukOth8');
-define('CONSUMER_KEY', 'kXKcOicjEgZTNNhQCEE2Ct4Alm41RTuK3KeQighTfE');
+define('CONSUMER_SECRET', 'kXKcOicjEgZTNNhQCEE2Ct4Alm41RTuK3KeQighTfE');
 // $host = $_SERVER['HTTP_HOST'];
 // $path = $_SERVER['SCRIPT_NAME'];
 // define('OAUTH_CALLBACK', $host );
