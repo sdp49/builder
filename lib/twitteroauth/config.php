1ee82a549d62031660bfdaeb46ab50e8bd1d9412
mpeshev <mario@peshev.net>
2013-01-03 21:06:52 +0200

    Update facebook sdk and twitteroauth as non-submodules but integrated

---
+++ lib/twitteroauth/config.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @file
+ * A single location to store configuration.
+ */
+
+#define('CONSUMER_KEY', '1OYM9QH9uEfX1tuh6hTfw');
+define('CONSUMER_KEY', 'a5Jihv3R0km2IsZmacsljQ');
+#define('CONSUMER_SECRET', '1dFNyFoJiCB8SmrPwZ9r1yNIBgcDbnsVnxFukOth8');
+define('CONSUMER_KEY', 'kXKcOicjEgZTNNhQCEE2Ct4Alm41RTuK3KeQighTfE');
+// $host = $_SERVER['HTTP_HOST'];
+// $path = $_SERVER['SCRIPT_NAME'];
+// define('OAUTH_CALLBACK', $host );
