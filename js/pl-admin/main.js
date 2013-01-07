765cb57c607d586c794da60c2f8ed3dbabf908d5
iantendick <itendick@placester.com>
2013-01-07 11:09:54 -0500

    Breaking out JS into two separate files...

--- js/pl-admin/main.js
+++ js/pl-admin/main.js
@@ -16,29 +16,7 @@ pl_admin_global = {};
 
 jQuery(document).ready(function($) {
 
-  $('#main-iframe').load(function() {
-    // Define the iframe document variable...
-    var iframe = this;
-    var iframeDOM = $(this).contents().contents();
-
-    // Append the escape arg to any links clicked from the iframe...
-    iframeDOM.find('a').on('click', function (event) {
-  	  event.preventDefault();
-
-  	  // Get original href of the link clicked...
-  	  var aHref = this.href;
-
-  	  // Depending on whether there are existing query args, append accordingly...
-  	  escapeArg = ( aHref.indexOf('?') !== -1 ? '&' : '?' ) + 'content=true';
-
-  	  var newHref = aHref + escapeArg;
-
-  	  console.log(newHref);
-  	  console.log(iframe);
-
-  	  iframe.src = newHref;
-    });
-  });
+  // ...
 
 });
 
