fa8fc732804a42dd212348638de1c336636d1209
mpeshev <mario@peshev.net>
2013-02-25 16:57:05 +0200

    Slightly styled Google+ field for Social screen

--- views/admin/social/googleplus.php
+++ views/admin/social/googleplus.php
@@ -1,6 +1,8 @@
 <div class="googleplus-wrapper">
+	<style type="text/css">
+		#pls_googleplus_id { width: 250px; }
+	</style>
 	<form id="pls_settings_form" action="options.php" method="POST">
-			<p>Enter your Google+ account ID</p>
 			<?php settings_fields( self::$social_setting_key ) ?>
 			<?php do_settings_sections( 'placester_social' ) ?>
 			
