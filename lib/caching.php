e33be63c7134d49d5caad8e63ccc65caa4bebc4a
iantendick <itendick@placester.com>
2013-04-25 16:03:32 -0400

    Updates...

--- lib/caching.php
+++ lib/caching.php
@@ -76,8 +76,8 @@ class PL_Cache {
 		// Allow caching as long as user is not an admin AND is not in the admin panel...
 		// return ( !current_user_can('manage_options') && !is_admin() );
 
-		// For now, refuse caching for ALL authenticated users...
-		return ( !is_user_logged_in() && !is_admin() );
+		// For now, refuse caching for ALL authenticated users + devs with debug turned on...
+		return ( !is_user_logged_in() && !is_admin() && !defined('PL_ENABLE_CACHE') );
 	}
 
 	public static function build_cache_key ($group, $func_args = array()) {
