3ce4cda64e899c69995ee70fafca08371e949e3f
Matt Barba <matt@placester.com>
2013-05-06 08:28:45 -0400

    remove inline styling. Add a few classes.
    Signed-off-by: Matt Barba <matt@placester.com>

--- views/frontend/saved-search-button.php
+++ views/frontend/saved-search-button.php
@@ -1,6 +1,6 @@
 <?php if (get_current_user_id()): ?>
-	<div>
-	  <a target="_blank" class="pls_save_search" href="#pl_saved_search_register_form" style="font-size: 16px; color: white; margin-top: 20px; font-weight: bold">Save this Search</a>
+	<div class="save_search_button_wrapper">
+	  <a target="_blank" class="pls_save_search" href="#pl_saved_search_register_form">Save this Search</a>
 	  <span id="pls_successful_saved_search" style="display: none">Search Successfully Saved! View it <a href="<?php echo PL_Membership::get_client_area_url(); ?>">here</a></span>
 	</div>	
 <?php else: ?>
