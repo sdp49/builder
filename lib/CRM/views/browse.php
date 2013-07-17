cc379718c60736868b6495f98200a35865725d36
iantendick <itendick@placester.com>
2013-07-17 16:04:31 -0400

    CRM Updates...

--- lib/CRM/views/browse.php
+++ lib/CRM/views/browse.php
@@ -18,7 +18,7 @@
 <div class="crm-browse-box">
 	<div class="browse-header">
 		<h2>Your site's CRM is integrated with <?php echo $display_name; ?></h2>
-		<a href="#" class="deactivate-button button-secondary">CRM Settings</a>
+		<a href="#" class="settings-button button-secondary">CRM Settings</a>
 	</div>
 
 	<!-- <div class="browse-logo">
