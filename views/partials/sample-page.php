6f6582e207c2243df935b4ebc6b88974853600b6
Matt Barba <matt@placester.com>
2013-08-06 11:48:00 -0400

    update to the styling of the set up complete overlay.
    Signed-off-by: Matt Barba <matt@placester.com>

--- views/partials/sample-page.php
+++ views/partials/sample-page.php
@@ -1,10 +1,9 @@
 <div id="searchpage-inner" class="hide">
 	<ul>
 		<?php if ($page):?>
-			<li>Check out your real estate search here: <a href="<?php echo get_permalink($page->ID)?>" target="_blank"><?php echo $page->post_title ?></a></li>
+			<p style="font-size: 23px; line-height: 1.2">We have created a real estate serach for you. Check it out <a href="<?php echo get_permalink($page->ID)?>" target="_blank">here</a></p>
 		<?php endif ?>
-		<li>Right now we're showing example listings. To turn this off, go <a href="<?php echo admin_url('admin.php?page=placester_settings') ?>" target="_blank">here</a>.</li>
-		<li>You can customize the way your real estate search looks (or create new real estate search pages) using shortcodes.</li>
-		<li>Checkout shortcodes <a href="<?php echo admin_url('admin.php?page=placester_shortcodes_shortcode_edit') ?>" target="_blank">here</a> and checkout a guide to using shortcodes <a href="https://placester.com/developers/placester-shortcode-overview/" target="_blank">here</a>.</li>
+		<p style="font-size: 14px;">The search is powered by Placester Shortcodes. Placester Shortcodes allow you to customize the way your real estate search looks (or create new search pages). Learn more about shortcodes <a href="https://placester.com/developers/placester-shortcode-overview/" target="_blank">here</a> or to view them, click <a href="<?php echo admin_url('admin.php?page=placester_shortcodes_shortcode_edit') ?>" target="_blank">here</a>.</p>
+		<p style="font-size: 14px; color: #f70820">Right now we're showing example listings. To turn this off, go <a href="<?php echo admin_url('admin.php?page=placester_settings') ?>" target="_blank">here</a>.</p>
 	</ul>
 </div>
