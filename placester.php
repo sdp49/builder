92dabacf71a480dd477c2f2a007feedb76db0509
iantendick <itendick@placester.com>
2013-09-03 02:18:24 -0400

    Updates...

--- placester.php
+++ placester.php
@@ -169,7 +169,7 @@ include_once('helpers/wordpress.php');
 include_once('helpers/education-com.php');
 include_once('helpers/caching.php');
 include_once('helpers/membership.php');
-include_once('helpers/saved-search.php');
+// include_once('helpers/saved-search.php');
 include_once('helpers/lead-capture.php');
 include_once('helpers/customizer.php');
 include_once('helpers/logging.php');
