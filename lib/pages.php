60f9660b5a93e3cb41332da571099fe4327a3ea6
Brendan Gannon <bgannon@placester.com>
2013-09-05 15:32:04 -0400

    Delete taxonomy terms/relationships when deleting all properties; use wp_delete_post when deleting single property

--- lib/pages.php
+++ lib/pages.php
@@ -1,9 +1,23 @@
 <?php 
 
 PL_Pages::init();
+/**
+ * @todo expose list of taxonomies in helpers/taxonomy.php and get list from there instead
+ */
 class PL_Pages {
 
 	public static $property_post_type = 'property';
+	public static $all_taxonomies = array(
+		'state',
+		'zip',
+		'city',
+		'neighborhood',
+		'street',
+		'beds',
+		'baths',
+		'half-baths',
+		'mlsid'
+	);
 
 	public static function init () {
 		add_action( 'init', array(__CLASS__, 'create_taxonomies') );
@@ -112,24 +126,85 @@ class PL_Pages {
         return $post_id;
 	}
 
+	/**
+	 * Deletes all properties and their associated post meta.
+	 * 
+	 * @return bool true if delete successful
+	 */
 	public static function delete_all () {
 		global $wpdb;
-    	$posts_table = $wpdb->prefix . 'posts';
-    	$results = $wpdb->get_results( "DELETE FROM $posts_table WHERE post_type = '" . self::$property_post_type . "'");
+
+		$q_ids = $wpdb->prepare("SELECT ID FROM $wpdb->posts WHERE post_type = %s", self::$property_post_type);
+		$prop_ids = $wpdb->get_col($q_ids);
+		$id_str = implode(',', $prop_ids);
+
+    	$results = $wpdb->query( "DELETE FROM $wpdb->posts WHERE ID IN ($id_str)");
+   
     	if (empty($results)) {
-    		return true;
+    		return false;
     	}
-    	return false;
+
+		$wpdb->query("DELETE FROM $wpdb->postmeta WHERE post_id IN ($id_str)");
+		self::delete_all_terms();
+
+    	return true;
 	}
 
+	/**
+	 * Given a name (property id), deletes the corresponding WP post and all associated data
+	 * 
+	 * @param  string $name post_name (property id)
+	 * @return bool 	true if successful
+	 */
 	public static function delete_by_name ($name) {
 		global $wpdb;
-    	$posts_table = $wpdb->prefix . 'posts';
-    	$results = $wpdb->get_results( "DELETE FROM $posts_table WHERE post_name = '".$name."' AND post_type = '" . self::$property_post_type . "'");
-    	if (empty($results)) {
-    		return true;
-    	}
-    	return false;	
+
+		if (!$name) {
+			return false;
+		}
+
+		$q_id = $wpdb->prepare("SELECT ID FROM $wpdb->posts WHERE post_name = %s AND post_type = %s", $name, self::$property_post_type);
+		$post_id_arr = $wpdb->get_col($q_id);
+
+		if (!is_array($post_id_arr) || count($post_id_arr) === 0) {
+			return false;
+		}
+
+		$post_id = $post_id_arr[0];
+		return (bool) wp_delete_post($post_id, true);
+	}
+
+	/**
+	 * Deletes all terms (and their relationships) associated with Property taxonomies.
+	 * 
+	 * @todo can we prompt Yoast to rebuild its sitemap?
+	 */
+	public static function delete_all_terms() {
+		global $wpdb;
+
+		$args = array(
+			'hide_empty' => false,
+			'fields' => 'ids'
+		);
+
+		$all_terms = get_terms( self::$all_taxonomies, $args );
+
+		if (!is_array($all_terms) || count($all_terms) === 0) {
+			return;
+		}
+
+		$term_str = implode(',', $all_terms);
+		$wpdb->query("DELETE FROM $wpdb->terms WHERE term_id IN ($term_str)");
+		$term_tax_ids = $wpdb->get_col("SELECT term_taxonomy_id FROM $wpdb->term_taxonomy WHERE term_id IN ($term_str)");
+
+		if (!is_array($term_tax_ids) || count($term_tax_ids) === 0) {
+			return;
+		}
+
+		$term_tax_str = implode(',', $term_tax_ids);
+		$wpdb->query("DELETE FROM $wpdb->term_taxonomy WHERE term_taxonomy_id IN ($term_tax_str)");
+		$q_term_rel = $wpdb->query("DELETE FROM $wpdb->term_relationships WHERE term_taxonomy_id IN ($term_tax_str)");
+
 	}
 
 	public static function create_taxonomies () {
