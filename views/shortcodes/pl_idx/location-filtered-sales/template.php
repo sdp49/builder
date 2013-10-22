743c33a2ef6fe307c211c634810256d0054c2be8
Galen Vinter <galen.vinter@gmail.com>
2013-10-22 17:20:30 -0400

    shortcodes - add top padding to location-filtered search button
    Signed-off-by: Galen Vinter <galen.vinter@gmail.com>

--- views/shortcodes/pl_idx/location-filtered-sales/template.php
+++ views/shortcodes/pl_idx/location-filtered-sales/template.php
@@ -46,7 +46,7 @@ $template = array(
     </div>
 
     <div class="pl_form-group pl_col pl_form-group--right">
-      <div class="pl_form-item pl_form-item--btn">
+      <div class="pl_form-item pl_form-item--btn pl_form-item--btn-top-bumper">
         <input type="submit" value="Search" />
       </div>
     </div>
