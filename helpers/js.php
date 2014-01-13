a33e214864b6f10a840e333a531bc6f917dad50b
iantendick <itendick@placester.com>
2014-01-13 02:49:10 -0500

    Conditionally load the saved-search.js based on whether or not the leads constant is defined...

--- helpers/js.php
+++ helpers/js.php
@@ -178,8 +178,11 @@ class PL_Js_Helper {
 		self::register_enqueue_if_not('datatables', trailingslashit(PL_JS_LIB_URL) . 'datatables/jquery.dataTables.js', array('jquery'));
 		// favorites/contact form
 		self::register_enqueue_if_not('membership', trailingslashit(PL_JS_PUB_URL) . 'membership.js', array('jquery'));
-		self::register_enqueue_if_not('saved-search', trailingslashit(PL_JS_PUB_URL) . 'saved-search.js', array('jquery'));
 		self::register_enqueue_if_not('general', trailingslashit(PL_JS_PUB_URL) . 'general.js', array('jquery'));
+
+		if (defined('PL_LEADS_ENABLED')) {
+			self::register_enqueue_if_not('saved-search', trailingslashit(PL_JS_PUB_URL) . 'saved-search.js', array('jquery'));
+		}
 
 		if ( PL_Option_Helper::get_demo_data_flag() && current_user_can('manage_options') ) {
 			self::register_enqueue_if_not('infobar', trailingslashit(PL_JS_PUB_URL) . 'infobar.js', array('jquery'));
